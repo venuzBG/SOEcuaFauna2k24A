f8551982dd9f8f802e0bb310564c2bf686fecf88
Sebas <sebas_alejandro@outlook.es>
2024-08-15 13:31:30 -0500

    Base de Datos Terminado

--- Script/DDL_ExamenII.sql
+++ Script/DDL_ExamenII.sql
@@ -7,6 +7,9 @@ Script: Insertando MER
 
 DROP TABLE IF EXISTS CatalogoTipo;
 DROP TABLE IF EXISTS Catalogo;
+DROP TABLE IF EXISTS Localidad;
+DROP TABLE IF EXISTS Alimento;
+DROP TABLE IF EXISTS Hormiga;
 
 CREATE TABLE catalogoTipo (
    IdCatalogoTipo   INTEGER NOT NULL PRIMARY KEY autoincrement 
@@ -20,9 +23,43 @@ CREATE TABLE catalogoTipo (
 CREATE TABLE Catalogo(
   IdCatalogo         INTEGER NOT NULL PRIMARY KEY autoincrement 
   ,IdCatalogoTipo    INTEGER NOT NULL REFERENCES CatalogoTipo(IdCatalogoTipo)
-  ,Nombre            VARCHAR(18) NOT NULL UNIQUE
+  ,Nombre            VARCHAR(18) NOT NULL 
 
   ,Estado           VARCHAR(1) NOT NULL DEFAULT('A')
   ,FechaCreacion    DATETIME DEFAULT(datetime('now','localtime'))
   ,FechaModifica    DATETIME
+);
+
+CREATE TABLE Localidad(
+    IdLocalidad             INTEGER NOT NULL PRIMARY KEY autoincrement
+    ,IdLocalidadPadre       INTEGER REFERENCES Localidad(IdLocalidad)
+    ,IdCTLocalidad          INTEGER REFERENCES Catalogo(IdCatalogo)
+    ,Nombre                 VARCHAR(18) NOT NULL 
+
+    ,Estado                 VARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion          DATETIME DEFAULT(datetime('now','localtime'))
+    ,FechaModifica          DATETIME
+);
+
+CREATE TABLE Alimento(
+  IdAlimento                  INTEGER NOT NULL PRIMARY KEY autoincrement
+  ,IdCTAlimento               INTEGER REFERENCES Catalogo(IdCatalogo)
+  ,Nombre                     VARCHAR(18) NOT NULL 
+
+  ,Estado                     VARCHAR(1) NOT NULL DEFAULT('A')
+  ,FechaCreacion              DATETIME DEFAULT(datetime('now','localtime'))
+  ,FechaModifica              DATETIME
+);
+
+CREATE TABLE Hormiga(
+  IdHormiga                     INTEGER NOT NULL PRIMARY KEY autoincrement
+  ,TipoHormiga                  INTEGER REFERENCES Catalogo(IdCatalogo)
+  ,Localidad                    INTEGER REFERENCES Localidad(IdLocalidad)
+  ,Sexo                         INTEGER REFERENCES Catalogo(IdCatalogo)
+  ,IngestaNativa                INTEGER REFERENCES Alimento(IdAlimento)
+  ,GenoAlimento                 INTEGER REFERENCES Alimento(IdAlimento)
+  
+  ,Estado                       VARCHAR(1) NOT NULL DEFAULT('A')
+  ,FechaNacimiento              DATETIME NOT NULL
+  ,FechaModifica                DATETIME
 );
--- Script/DML_ExamenII.sql
+++ Script/DML_ExamenII.sql
@@ -1,21 +1,76 @@
 -- database: ../DataBase/DB_ExamenII.sqlite
 
 INSERT INTO CatalogoTipo
-(Nombre) VALUES
-('TipoHormiga')
-,('Sexo')
-,('LocalidadTipo');
+ (Nombre            ) VALUES
+ ('TipoHormiga'     )
+,('Sexo'            )
+,('LocalidadTipo'   )
+,('Alimento'        );
 
 INSERT INTO Catalogo
-(IdCatalogoTipo  ,Nombre    ) VALUES
+(IdCatalogoTipo  ,Nombre            ) VALUES
+(1               ,'Reina'           )
+,(1              ,'Larva'           )
+,(1              ,'Rastrera'        )
+,(1              ,'Soldado'         )
+,(1              ,'Obrera'          )
+,(1              ,'Macho'           )
+,(1              ,'Aladas'          )
+,(2              ,'Macho'           )
+,(2              ,'Hembra'          )
+,(2              ,'Asexual'         )
+,(3              ,'Pais'            )
+,(3              ,'Region'          )
+,(3              ,'Provincia'       )
+,(4              ,'IngestaNativa'   )
+,(4              ,'GenoAlimento'    );
 
-(1               ,'Reina')
-,(1              ,'Larva')
-,(1              ,'Soldado')
-,(1              ,'Rastrera')
-,(1              ,'Femenino')
-,(1              ,'Otro')
-,(3              ,'Pais')
-,(3              ,'Region')
-,(3              ,'Provincia');
+INSERT INTO Localidad
+(IdLocalidadPadre  ,IdCTLocalidad           ,Nombre    ) VALUES
+(NULL              ,11                      ,'Ecuador' )
+,(1                ,12                      ,'Costa'   )
+,(1                ,12                      ,'Sierra'  )
+,(1                ,12                      ,'Oriente' );
 
+INSERT INTO Localidad
+(IdLocalidadPadre  ,IdCTLocalidad           ,Nombre                          ) VALUES
+ (1                ,12                      ,'Insular'                       ) 
+ 
+,(2                ,13                      ,'Esmeraldas'                    )
+,(2                ,13                      ,'Santo Domingo de los Tsáchilas')
+,(2                ,13                      ,'Manabí'                        )
+,(2                ,13                      ,'Los Ríos'                      )
+,(2                ,13                      ,'Guayas'                        )
+,(2                ,13                      ,'Santa Elena'                   )
+,(2                ,13                      ,'El Oro'                        )
+
+,(3                ,13                      ,'Carchi'                        )
+,(3                ,13                      ,'Imbabura'                      )
+,(3                ,13                      ,'Pichincha'                     )
+,(3                ,13                      ,'Cotopaxi'                      )
+,(3                ,13                      ,'Tungurahua'                    )
+,(3                ,13                      ,'Bolívar'                       )
+,(3                ,13                      ,'Chimborazo'                    )
+,(3                ,13                      ,'Cañar'                         )
+,(3                ,13                      ,'Azuay'                         )
+,(3                ,13                      ,'Loja'                          )
+
+,(4                ,13                      ,'Sucumbíos'                     )
+,(4                ,13                      ,'Napo'                          )
+,(4                ,13                      ,'Orellana'                      )
+,(4                ,13                      ,'Pastaza'                       )
+,(4                ,13                      ,'Morona Santiago'               )
+,(4                ,13                      ,'Zamora Chinchipe'              )
+
+
+,(5                ,13                      ,'Galápagos'                     );
+
+INSERT INTO Alimento
+(IdCTAlimento             ,Nombre    ) VALUES
+(14                       ,'Carnívoro'   ) 
+,(14                      ,'Herbívoro'   ) 
+,(14                      ,'Omnívoro'    ) 
+,(14                      ,'Insectívoros') 
+,(15                      ,'X'           ) 
+,(15                      ,'XX'          ) 
+,(15                      ,'XY'          );
